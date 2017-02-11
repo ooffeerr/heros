0a40e373fea1a7d5784bcdf6ff807006ae546c8f
Ofer Schonberger <ofer.schonberger@avg.com>
2017-02-11 23:13:26 +0200

    selection seems to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,17 +27,21 @@ template:`
   <h2>My Heroes</h2>
 <ul class="heroes">
     <!-- each hero goes here -->
-    <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+    <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero"
+    (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
 
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
   <div>
     <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
   </div>
+</div>
+
   `,
   styles: [`
   .selected {
@@ -94,9 +98,10 @@ export class AppComponent {
   name = 'Tour of Heroes';
   heroes = HEROES;
 
-  	hero: Hero = {
-  	id: 1,
-  	name: 'Windstorm'
-  };
+  	selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 }
 
