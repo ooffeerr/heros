a441e6b82b31b36e2221b1296d71477f547b8af6
Ofer Schonberger <ofer.schonberger@avg.com>
2017-02-15 14:14:20 +0200

    extracted a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 
 const HEROES: Hero[] = [
@@ -32,15 +28,8 @@ template:`
         <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
+<my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
-  <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  </div>
-</div>
 
   `,
   styles: [`
