304816d014a01578e880e602f2f5e4e865a21d30
Ofer Schonberger <ofer.schonberger@avg.com>
2017-02-20 17:12:03 +0200

    extracted the heros componenet towards navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router'
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heros.component';
+import { HeroService } from './hero.service';
+
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent,HeroesComponent, HeroDetailComponent ],
+
+  providers:	[ HeroService ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
