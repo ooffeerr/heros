a78ac4e0da4cf646a2a56703d5091925f8052619
Ofer Schonberger <ofer.schonberger@avg.com>
2017-04-04 16:49:42 +0300

    search seems to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router'
-import { AppRoutingModule } from './app-rounting.module'
 import { HttpModule }    from '@angular/http';
-
-
+import { AppRoutingModule } from './app-rounting.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heros.component';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
-
-
-
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heros.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService),AppRoutingModule],
-  declarations: [ AppComponent,DashboardComponent, HeroesComponent, HeroDetailComponent ],
-
-  providers:	[ HeroService ],
-  bootstrap:    [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
